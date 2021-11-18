f94e138689e5cda2c81553169cf397fa73d3a357
Edvan Monteiro <87432834+edvaaaan@users.noreply.github.com>
2021-11-18 18:50:07 -0300

    Add files via upload

---
+++ extra/extra3.py
@@ -0,0 +1,11 @@
+# IF, ELSE E IF ELSE 
+
+idade = int(input('Digite sua idade: '))
+if idade < 0:
+    print('Você nem nasceu ainda!')
+elif idade > 18: 
+    print('Você é adulto!')
+else:
+    print('Você é criança!')
+
+# igual(==); e(and); ou(or); reverso(not)
