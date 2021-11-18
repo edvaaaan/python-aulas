d6d3651beb9a689f95388523467c95217ff99a22
Edvan Monteiro <87432834+edvaaaan@users.noreply.github.com>
2021-11-18 18:20:43 -0300

    Add files via upload

---
+++ extra/extra1.py
@@ -0,0 +1,13 @@
+# STRINGS EM PYTHON
+
+word = 'Edvan'
+print(len(word)) # -> TAMANHO DA PALAVRA
+print(word.find('a')) # -> DIZ EM QUE POSIÇÃO ESTÁ A LETRA
+print(word.capitalize()) # -> PRIMEIRA LETRA EM MAIUSCULA
+print(word.upper()) # -> DEIXA EM CAIXA ALTA
+print(word.lower()) # -> DEIXA EM CAIXA BAIXA
+print(word.isalpha()) # -> VE SE É ALFABETICA
+print(word.isdigit()) # -> VE SE É DIGITO
+print(word.count('d')) # -> CONTA O NUMERO DE LETRAS
+print(word.replace('a', 'x')) # -> FAZ UMA TROCA DE LETRAS
+print(word*3) # -> ESCREVE O NOME X VEZES 
---
+++ extra/extra2.py
@@ -0,0 +1,9 @@
+# STRINGS EM PYTHON 2
+# indexing[] ou slice()
+# [start:stop:step]
+
+nome = 'Radical Edvan'
+pnome = nome[0:7]
+snome = nome[8:13]
+enome = nome[0:7:2] 
+print(enome)
